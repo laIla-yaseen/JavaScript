68f252ed87df364faf319d8751bc3ff19d069ca0
unknown <lailaeasen2244@gmail.com>
2024-06-24 04:04:48 +0300

    opp

---
+++ 23-6/maid.js
@@ -0,0 +1,34 @@
+class Car {
+    mark;
+    model;
+    
+    
+    started (){
+        console.log("the car has started")
+    }
+    
+
+
+ constructor(mark,model){
+    this.mark;
+    this.model;
+
+ }
+}
+
+let ca = new Car()
+ca.mark = "jo",
+ca.model= "ka"
+
+console.log(ca)
+
+ca.started()
+
+
+class ElectricCar extends Car{
+    Battery_size ;
+}
+
+
+
+
